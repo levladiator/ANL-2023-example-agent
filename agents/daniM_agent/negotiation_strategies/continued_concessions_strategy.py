a91c75039b944298e712283c250ff53f429361e9
Vlad-Ioan Dragutoiu <v.dragutoiu-1@student.tudelft.nl>
2025-04-02 15:46:50 +0300

    Fix bug

--- agents/daniM_agent/negotiation_strategies/continued_concessions_strategy.py
+++ agents/daniM_agent/negotiation_strategies/continued_concessions_strategy.py
@@ -7,5 +7,17 @@ class ContinuedConcessionsStrategy(NegotiationStrategy):
 
     def score_bid(self, bid: Bid, agent) -> float:
         if len(agent.own_bids) > 2:
+            own_last_bid_utility = float(agent.profile.getUtility(agent.own_bids[-1]))
+            current_bid_utility = float(agent.profile.getUtility(bid))
+            own_delta = current_bid_utility - own_last_bid_utility
+
+            opponent_last_bid_utility = agent.opponent_model.get_predicted_utility(agent.own_bids[-1])
+            opponent_current_bid_utility = agent.opponent_model.get_predicted_utility(bid)
+            opponent_delta = opponent_current_bid_utility - opponent_last_bid_utility
+
+            if own_delta >= 0 or opponent_delta <= 0:
+                return 0
+
             return self.score_bid_for_social_welfare(bid, agent)
-        return agent.profile.getUtility(bid)
+        else:
+            return agent.profile.getUtility(bid)
--- agents/daniM_agent/negotiation_strategies/negotiation_strategy.py
+++ agents/daniM_agent/negotiation_strategies/negotiation_strategy.py
@@ -12,20 +12,6 @@ class NegotiationStrategy(ABC):
         """
 
     def score_bid_for_social_welfare(self, bid: Bid, agent) -> float:
-        if len(agent.own_bids) < 2:
-            return float(agent.profile.getUtility(bid))
-
-        own_last_bid_utility = float(agent.profile.getUtility(agent.own_bids[-1]))
-        current_bid_utility = float(agent.profile.getUtility(bid))
-        own_delta = current_bid_utility - own_last_bid_utility
-
-        opponent_last_bid_utility = agent.opponent_model.get_predicted_utility(agent.own_bids[-1])
-        opponent_current_bid_utility = agent.opponent_model.get_predicted_utility(bid)
-        opponent_delta = opponent_current_bid_utility - opponent_last_bid_utility
-
-        if own_delta >= 0 or opponent_delta <= 0:
-            return 0
-
         progress = agent.progress.get(int(time() * 1000))
 
         our_utility = float(agent.profile.getUtility(bid))
